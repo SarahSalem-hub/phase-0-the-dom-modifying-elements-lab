c06e14c31e52bda1e6405bd63698dbc6df06ce39
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2023-01-02 13:52:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const element = document.getElementById("main")
+element.remove()
+
+const h1 = document.createElement("h1")
+let newHeader = h1
+
+h1.setAttribute("id","victory")
+h1.innerHTML = "Sarah is the champion"
